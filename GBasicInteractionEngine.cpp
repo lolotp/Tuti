c5161f69919a71eeac755904d3ed799f72eb5fef
lolotp <lolo_vn@yahoo.co.uk>
2012-12-25 22:19:10 +0800

    restructure event/action subscription to improve efficiency

--- GBasicInteractionEngine.cpp
+++ GBasicInteractionEngine.cpp
@@ -2,12 +2,19 @@
 
 GBasicInteractionEngine::GBasicInteractionEngine() {
     eventList.resize(3);
+    for (unsigned i = 0; i < 3; i++) {
+        eventList[i] = new GActionNode();
+        eventList[i]->nxt = eventList[i]->prev = NULL;
+        eventList[i]->action = NULL;
+    }
 };
 
 GEventID GBasicInteractionEngine::registerEvent() {
     int ret = eventList.size();
     eventList.resize(eventList.size() + 1);
-    eventList[ret] = NULL;
+    eventList[ret] = new GActionNode();
+    eventList[ret]->nxt = eventList[ret]->prev = NULL;
+    eventList[ret]->action = NULL;
     return ret;
 };
 
@@ -19,28 +26,25 @@ SubsID GBasicInteractionEngine::subscribeEvent(GEventID id, GAction *action) {
     if (id >= eventList.size()) return 0;
     GActionNode* newNode = new GActionNode();
     newNode->action = action;
-    if (eventList[id] != NULL)
-        eventList[id]->prev = newNode;
-    newNode->nxt  = eventList[id];
-    newNode->prev = NULL;
-    eventList[id] = newNode;
+    newNode->nxt = eventList[id]->nxt;    
+    if (eventList[id]->nxt != NULL)
+        eventList[id]->nxt->prev = newNode;    
+    newNode->prev = eventList[id];
+    eventList[id]->nxt = newNode;
     return (SubsID) newNode;
 };
 
 void GBasicInteractionEngine::unsubscribeEvent(SubsID subsID) {
     GActionNode *node = (GActionNode*) subsID;
     GActionNode *prev = node->prev;
-    GActionNode *nxt = node->nxt;
-    for (unsigned i = 0; i < eventList.size(); i++) {
-        if (eventList[i] == node) eventList[i] = nxt;
-    }
+    GActionNode *nxt = node->nxt;    
     if (prev != NULL) prev->nxt = nxt;
     if (nxt != NULL) nxt->prev = prev;
     delete node;
 };
 
 void GBasicInteractionEngine::processWorld() {    
-    GActionNode *head = eventList[FrameEvent];
+    GActionNode *head = eventList[FrameEvent]->nxt;
     while (head != NULL) {
         head->action->doAction(NULL);
         std::cout<<"processing events"<<std::endl;
